0916a4f141a7f0c0adea20c4d9c2dde86ef52f5e
Aggelos Kolaitis <neoaggelos@gmail.com>
2016-10-07 21:05:34 +0300

    fixed paths in Android.mk

--- android-project/jni/src/Android.mk
+++ android-project/jni/src/Android.mk
@@ -9,19 +9,19 @@ SDL_PATH := ../SDL
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-	$(LOCAL_PATH)/../../../src/difficulty.cpp \
-	$(LOCAL_PATH)/../../../src/entername.cpp \
-	$(LOCAL_PATH)/../../../src/game.cpp \
-	$(LOCAL_PATH)/../../../src/highscores.cpp \
-	$(LOCAL_PATH)/../../../src/main.cpp \
-	$(LOCAL_PATH)/../../../src/render.cpp \
-	$(LOCAL_PATH)/../../../src/settings.cpp \
-	$(LOCAL_PATH)/../../../src/shape.cpp \
-	$(LOCAL_PATH)/../../../src/super.cpp \
-	$(LOCAL_PATH)/../../../src/textrenderer.cpp \
-	$(LOCAL_PATH)/../../../src/tutorial.cpp \
-	$(LOCAL_PATH)/../../../src/audiodata.cpp \
-	$(LOCAL_PATH)/../../../src/util.cpp
+	../../../src/difficulty.cpp \
+	../../../src/entername.cpp \
+	../../../src/game.cpp \
+	../../../src/highscores.cpp \
+	../../../src/main.cpp \
+	../../../src/render.cpp \
+	../../../src/settings.cpp \
+	../../../src/shape.cpp \
+	../../../src/super.cpp \
+	../../../src/textrenderer.cpp \
+	../../../src/tutorial.cpp \
+	../../../src/audiodata.cpp \
+	../../../src/util.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_mixer SDLU
 
